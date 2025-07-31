0227f797ed64197e6b654dea1d1939cbcded7134
Aidimys <mklirill939@gmail.com>
2025-07-31 15:25:50 +0300

    Revert "All ready"
    This reverts commit 69535eb358b44f3236a156ddae575c2940dcd3ea.

--- README.md
+++
@@ -1,9 +0,0 @@
-https://drive.google.com/file/d/1C148FRnWfXVoRDslDWcYac3bEhebdIAV/view?pli=1
-
-Кастомизируемая библиотека React-компонентов с TypeScript и Storybook для документации и разработки.
-
-Установка:
-
-npm install km-custom-components-lib
-
-import { Button, TextField, Checkbox } from 'your-component-library';
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Button } from './components/Button/Button';
-export { default as TextField } from './components/TextField/TextField';
-export { default as Select } from './components/Select/Select';
-export { default as Checkbox } from './components/Checkbox/Checkbox';
-export { default as Switch } from './components/Switch/Switch';
